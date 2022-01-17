09380d6f894b90f8987b0a366957c0e4612ade59
Didi Dodson <mdodson55@gmail.com>
2022-01-17 09:37:48 -0700

    Update setup files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,14 @@
 # it.
 require 'simplecov'
 SimpleCov.start 'rails'
-SimpleCov.add_filter ['spec', 'config']
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/config/"
+  add_filter "app/channels/application_cable/channel.rb"
+  add_filter "app/channels/application_cable/connection.rb"
+  add_filter "app/jobs/application_job.rb"
+  add_filter "app/mailers/application_mailer.rb"
+end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
