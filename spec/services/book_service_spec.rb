764cae5bb7bed2f7c01597ffcd21f350295d9381
Didi Dodson <mdodson55@gmail.com>
2022-01-18 14:20:02 -0700

    Add sad path testing

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe BookService do
   describe 'class methods' do
     it 'accesses book service' do
       location = 'Denver, CO'
+      quantity = 5
 
-      result = BookService.get_books(location)
+      result = BookService.get_books(location, quantity)
 
       expect(result).to have_key(:numFound)
       expect(result[:docs][0]).to have_key(:title)
