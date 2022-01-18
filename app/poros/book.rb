e3ad233dad35ed94a518a6d94e3de6519878bf68
Didi Dodson <mdodson55@gmail.com>
2022-01-18 16:21:49 -0700

    Refactor book poro

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -11,13 +11,24 @@ class Book
     @type = "books"
     @total_books_found = data[:numFound]
     @destination = location
-    @current_weather = {
-      summary: current_weather[:conditions],
-      temperature: current_weather[:temperature]}
-    @books = data[:docs].take(quantity.to_i).map do |book|
-      {isbn: book[:isbn],
-      title: book[:title],
-      publisher: book[:publisher]}
+    @current_weather = current_weather_hash(current_weather)
+    @books = books_array(data[:docs], quantity)
+  end
+
+  def current_weather_hash(weather_data)
+    {
+      summary: weather_data[:conditions],
+      temperature: weather_data[:temperature]
+    }
+  end
+
+  def books_array(book_data, quantity)
+    book_data.take(quantity.to_i).map do |book|
+      {
+        isbn: book[:isbn],
+        title: book[:title],
+        publisher: book[:publisher]
+      }
     end
   end
 end
